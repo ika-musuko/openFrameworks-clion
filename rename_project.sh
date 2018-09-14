5881ecd074e8597dab2cec0083a81d50ab324605
sherwyn <sherwyn.sen@gmail.com>
2018-09-13 23:41:33 -0700

    fix bug in rename_project.sh that caused only one file to be renamed - forgot how to loop in bash LOL

--- rename_project.sh
+++ rename_project.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+# files that need to be edited to change the project name
+rename_these=( CMakeLists.txt .idea/runConfigurations/oFDebug.xml )
+
+# get the current project name from the buffer file
 project_name=$(<.project_name)
-rename_these=(CMakeLists.txt .idea/runConfigurations/oFDebug.xml)
+
+# loop over all the files and rename them using ex
 echo "renaming $project_name to $1..."
-for f in $rename_these
+for f in "${rename_these[@]}"
 do
-    ex -s +%s/$project_name/$1/g +x $f
+    echo modifying "$f"...
+    ex -s +%s/$project_name/$1/g +x "$f"
 done
+
+# change the project name in the buffer file for future renaming
 echo $1 > .project_name
